94ff6ab4abf1e8601ff9d148937daa34f98c9848
Toni <toni.giorgino@gmail.com>
2026-03-24 16:14:26 +0100

    Track prebuilt frontend assets

---
+++ streamlit_molstar/frontend/build/precache-manifest.2b6bc4b6211c1b55b3322d4ec7d3129e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b1ff876afea1c58615264a1b153ba0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbb57179f1c0e46d95bd",
+    "url": "./static/css/2.5b1a2ecb.chunk.css"
+  },
+  {
+    "revision": "4317233cb8fa12a23b4a",
+    "url": "./static/css/main.2a41cbb3.chunk.css"
+  },
+  {
+    "revision": "cbb57179f1c0e46d95bd",
+    "url": "./static/js/2.139c2fb7.chunk.js"
+  },
+  {
+    "revision": "df087e3b51cab167e93df344458ea8f1",
+    "url": "./static/js/2.139c2fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4317233cb8fa12a23b4a",
+    "url": "./static/js/main.7ae102e2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.7ae102e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54847fc9bcbb4da411ae",
+    "url": "./static/js/runtime-main.bad65001.js"
+  }
+]);
