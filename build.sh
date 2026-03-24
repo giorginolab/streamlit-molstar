e66f5361f766c7f6a42de920360c050c9adfbc2d
Toni <toni.giorgino@gmail.com>
2026-03-24 16:12:30 +0100

    Set legacy OpenSSL flag in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -ex
-#export NODE_OPTIONS="--openssl-legacy-provider"
+export NODE_OPTIONS="--openssl-legacy-provider"
 export BROWSER=none
 (cd streamlit_molstar/frontend && npm i --legacy-peer-deps && npm run build)
 (cd streamlit_molstar/docking/frontend && npm i --legacy-peer-deps && npm run build)
